c12faa865a7579be07ce0c429eb12cb701eeef41
ludemeula <ludemeula@gmail.com>
2019-07-01 16:35:18 -0300

    Adding grid of gifs

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -9,7 +9,6 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-
   String _search;
   int _offset = 0;
 
@@ -17,9 +16,11 @@ class _HomePageState extends State<HomePage> {
     http.Response response;
 
     if (_search == null)
-      response = await http.get('https://api.giphy.com/v1/gifs/trending?api_key=L4rYZJrrse1cf6DGO9PFOkwpYc6HEOQN&limit=25&rating=G');
+      response = await http.get(
+          'https://api.giphy.com/v1/gifs/trending?api_key=L4rYZJrrse1cf6DGO9PFOkwpYc6HEOQN&limit=25&rating=G');
     else
-      response = await http.get('https://api.giphy.com/v1/gifs/search?api_key=L4rYZJrrse1cf6DGO9PFOkwpYc6HEOQN&q=$_search&limit=25&offset=$_offset&rating=G&lang=en');
+      response = await http.get(
+          'https://api.giphy.com/v1/gifs/search?api_key=L4rYZJrrse1cf6DGO9PFOkwpYc6HEOQN&q=$_search&limit=25&offset=$_offset&rating=G&lang=en');
 
     return json.decode(response.body);
   }
@@ -38,7 +39,8 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.black,
-        title: Image.network('https://developers.giphy.com/static/img/dev-logo-lg.7404c00322a8.gif'),
+        title: Image.network(
+            'https://developers.giphy.com/static/img/dev-logo-lg.7404c00322a8.gif'),
         centerTitle: true,
       ),
       backgroundColor: Colors.black,
@@ -50,13 +52,54 @@ class _HomePageState extends State<HomePage> {
               decoration: InputDecoration(
                   labelText: 'Pesquise Aqui!',
                   labelStyle: TextStyle(color: Colors.white),
-                  border: OutlineInputBorder()
-              ),
+                  border: OutlineInputBorder()),
               style: TextStyle(color: Colors.white),
+            ),
+          ),
+          Expanded(
+            child: FutureBuilder(
+              future: _getSearch(),
+              builder: (context, snapshot) {
+                switch (snapshot.connectionState) {
+                  case ConnectionState.waiting:
+                  case ConnectionState.none:
+                    return Container(
+                      height: 200,
+                      width: 200,
+                      child: CircularProgressIndicator(
+                        valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+                        strokeWidth: 5,
+                      ),
+                    );
+                  default:
+                    if (snapshot.hasError)
+                      return Container();
+                    else
+                      return _createGifTable(context, snapshot);
+                }
+              },
             ),
           )
         ],
       ),
+    );
+  }
+
+  _createGifTable(BuildContext context, AsyncSnapshot snapshot) {
+    return GridView.builder(
+      padding: EdgeInsets.all(10),
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2, crossAxisSpacing: 10, mainAxisSpacing: 10),
+      itemCount: snapshot.data['data'].length,
+      itemBuilder: (context, index) {
+        return GestureDetector(
+          child: Image.network(
+            snapshot.data['data'][index]['images']['fixed_height']['url'],
+            height: 300,
+            fit: BoxFit.cover,
+          ),
+        );
+      },
     );
   }
 }
