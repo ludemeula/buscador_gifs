e844f41a1877819a8117d834b606c90d319d1ffa
ludemeula <ludemeula@gmail.com>
2019-07-02 17:01:34 -0300

    Creating Gif Page

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:buscador_gifs/ui/home_page.dart';
-import 'package:buscador_gifs/ui/gif_page.dart';
-
 
 void main() {
  runApp(MaterialApp(
-   home: GifPage(),
+   home: HomePage(),
    theme: ThemeData(hintColor: Colors.white),
  ));
 }
